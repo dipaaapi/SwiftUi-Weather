682dd06ede22fc9418378cc37ee34126bfcc7944
KuyaMoEd <31286911+dipaaapi@users.noreply.github.com>
2022-01-03 16:59:01 +0800

    initial commit

---
+++ SwiftUi-Weather/SwiftUi_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_WeatherApp.swift
+//  SwiftUi-Weather
+//
+//  Created by Repub on 9/29/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUi-Weather/WeatherButton.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherButton.swift
+//  SwiftUi-Weather
+//
+//  Created by Repub on 9/29/21.
+//
+
+import SwiftUI
+
+struct WeatherButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10)
+    }
+}
